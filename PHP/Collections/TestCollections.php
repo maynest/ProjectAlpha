513d02ac10f8dd85615da7ba7781a522699bc7c4
JoeVieira <joe@myfonts.com>
2016-05-23 19:49:45 -0400

    flush out prompt for collections

--- PHP/Collections/TestCollections.php
+++ PHP/Collections/TestCollections.php
@@ -1,36 +1,19 @@
 <?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: vieiraj
+ * Date: 5/23/16
+ * Time: 6:53 PM
+ */
 
-require_once 'ActualCollection.php';
-
-//Prompt:
-//
-
-$collection = new ActualCollection();
-
-$collection->addItem(new stdClass(), "key");
-$collection->addItem(new stdClass(), "key8");
-$collection->addItem(new stdClass(), "key2");
-
-$collection->delItem("key2");
-
-foreach ($collection as $key => $item) {
-	print $key."\n";
-	print_r($item);
-}
-
-$sortedCollection = ActualSortedCollection::fromUnsorted($collection, SortedCollection::SORT_DESC);
-print "\nSorting...\n\n";
-
-$sortedCollection->addItem(new stdClass(), "key2");
-
-foreach ($sortedCollection as $key => $item) {
-	print $key."\n";
-	print_r($item);
-}
-
-//should output
-/*
-
+/* Prompt:
+ *
+ * Implement ActualCollection and ActualSortedCollection
+ *  such that the example application below outputs the expected results
+ * 
+ * This implementation should be general purpose, scalable and readable. 
+ *
+ * example output: 
 key
 stdClass Object
 (
@@ -54,5 +37,31 @@ key
 stdClass Object
 (
 )
-  
-*/
+ *
+ */
+
+require_once 'ActualCollection.php';
+
+
+$collection = new ActualCollection();
+
+$collection->addItem(new stdClass(), "key");
+$collection->addItem(new stdClass(), "key8");
+$collection->addItem(new stdClass(), "key2");
+
+$collection->delItem("key2");
+
+foreach ($collection as $key => $item) {
+	print $key."\n";
+	print_r($item);
+}
+
+$sortedCollection = ActualSortedCollection::fromUnsorted($collection, SortedCollection::SORT_DESC);
+print "\nSorting...\n\n";
+
+$sortedCollection->addItem(new stdClass(), "key2");
+
+foreach ($sortedCollection as $key => $item) {
+	print $key."\n";
+	print_r($item);
+}
