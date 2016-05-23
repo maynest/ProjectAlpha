e62cc73d1e8da21efa09d09b35675e64d45c2ece
JoeVieira <joe@myfonts.com>
2016-05-23 19:30:57 -0400

    Stub PHP ArrayFlatten

---
+++ PHP/Arrays/ArrayFlatten.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: vieiraj
+ * Date: 5/23/16
+ * Time: 6:53 PM
+ */
+
+/* Prompt:
+ * Flatten the output of an array structure such that all values inside the array structure
+ *  can be output with print
+ *
+ * Example input: [1,2,3,[4,5,6],[7,[8,9]]]
+ * Example output: 1,2,3,4,5,6,7,8,9
+ *
+ * Your solution should be scalable, concise and readable.
+ *
+ */
+
+
+function flattenArray($array) {
+
+}
+
+
+
+
+
+
+print flattenArray();
