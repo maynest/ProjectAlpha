0a8d54490d61456b86f84e845a75444390dd42e4
Joe Vieira <joe@myfonts.com>
2016-05-24 10:34:40 -0400

    Update ActualCollection.php

--- PHP/Collections/ActualCollection.php
+++ PHP/Collections/ActualCollection.php
@@ -6,40 +6,6 @@
  * Time: 12:52 PM
  */
 
-/* Prompt:
- *
- * Implement ActualCollection and ActualSortedCollection
- *  such that the example application below outputs the expected results
- *
- * This implementation should be general purpose, scalable and readable.
- *
- * example output:
-key
-stdClass Object
-(
-)
-key8
-stdClass Object
-(
-)
-
-Sorting...
-
-key8
-stdClass Object
-(
-)
-key2
-stdClass Object
-(
-)
-key
-stdClass Object
-(
-)
- *
- */
-
 require_once 'iCollection.php';
 
 class ActualCollection implements Collection
