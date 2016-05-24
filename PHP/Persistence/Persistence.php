1fadfeee1172348e4b185d5b9664bdcd312f4e05
Mark Berube <mark@myfonts.com>
2016-05-24 13:01:39 -0400

    adding comments to php file

--- PHP/Persistence/Persistence.php
+++ PHP/Persistence/Persistence.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Implement calculateRoot that calculates the multiplicative digital root of the $number argument as an integer.
+ *
+ * example output:
+ *
+ * The multiplicative digital root of 7500 is 0.
+ * The multiplicative digital root of 123 is 6.
+ * The multiplicative digital root of 1337 is 8.
+ *
+ */
+
 $inputArray = [7500, 123, 1337];
 $printArray = [];
 
@@ -21,4 +32,3 @@ function calculateRoot($number) {
 foreach($printArray as $result) {
 	echo "The multiplicative digital root of ".$result['input']." is ".$result['result'].".".PHP_EOL;
 }
-
